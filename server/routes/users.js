4d73733ca3db8d6d6c4c205fbc05bd1a150b72f9
charliesands <cwsands3@gmail.com>
2018-08-24 13:25:22 -0600

    user routes

---
+++ server/routes/users.js
@@ -0,0 +1,24 @@
+let router = require('express').Router()
+let Users = require('../models/User')
+
+router.post('/login', (req, res, next) => {
+  Users.findOne({
+    username: req.body.username,
+    pin: req.body.pin
+  }).then(user => {
+    if (user) {
+      return res.send(user)
+    }
+    return res.status(401).send({
+      error: "Invalid Login"
+    })
+  })
+})
+
+router.post('/register', (req, res, next) => {
+  Users.create(req.body).then(user => {
+    res.send(user)
+  }).catch(next)
+})
+
+module.exports = router
