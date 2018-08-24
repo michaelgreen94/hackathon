ebd7f83b91c3188ea26d5299982f97e7a29add5c
Madison Abegglen <madison.abegglen@gmail.com>
2018-08-24 12:03:04 -0600

    comment viewModel & template

--- www/app/models/user.js
+++ www/app/models/user.js
@@ -2,6 +2,6 @@ export default class User {
     constructor(data) {
         this.userName = data.username
         this.pin = data.pin
-        this._id = data._id
+        this.userId = data._id
     }
 }
