3c837dd65569a0a6c826eb92eda16adbab40e280
charliesands <cwsands3@gmail.com>
2018-08-24 13:32:34 -0600

    index update

--- server/index.js
+++ server/index.js
@@ -12,10 +12,19 @@ server.use(express.static(__dirname + '/../www/'))
 
 //ROUTES
 let postRoutes = require('./routes/posts')
+let commentRoutes = require('./routes/comments')
+let userRoutes = require('./routes/users')
 
 server.use('/auth', postRoutes)
+server.use('/api/comments', commentRoutes)
+server.use('api/users', userRoutes)
 
-
+server.use('api/*', (err, req, res, next) => {
+  res.status(400).send(err)
+})
+server.use('*', (req, res, next) => {
+  res.status(404).send('<h1>404</h1>')
+})
 
 
 server.listen(port, () => {
