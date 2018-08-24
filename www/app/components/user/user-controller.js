c94d3e0722a4818e387ed7bdf8b35f0f976b2c99
Madison Abegglen <madison.abegglen@gmail.com>
2018-08-24 14:31:12 -0600

    create user form

--- www/app/components/user/user-controller.js
+++ www/app/components/user/user-controller.js
@@ -1,10 +1,26 @@
 import Store from "../../store/store.js"
 
+
 let store = new Store()
+
+let elem = document.getElementById('app')
+
+function draw() {
+
+}
 
 export default class UserController {
     constructor() {
-        console.log("hello")
+
+    }
+
+    login(e) {
+        e.preventDefault();
+        let creds = {
+            username: e.target.username.value,
+            pin: e.target.pin.value
+        }
+        store.login(creds, draw)
     }
 
 }
