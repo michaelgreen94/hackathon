7cdf1af4ec1bd725b29cb8e4c123e4bc722d210b
CaitlinCodes <caiit@me.com>
2018-08-24 23:29:25 -0600

    working with upvotes

--- www/app/store/store.js
+++ www/app/store/store.js
@@ -97,6 +97,12 @@ export default class Store {
             .then(getPosts)
     }
 
+    upvote(upvote, getPosts) {
+        upvote.username = state.user.userName
+        upvote.userId = state.user.userId
+        server.post("/api/posts/userId/upvote", upvote)
+            .then(getPosts)
+    }
 
     constructor() {
         if (store) {
